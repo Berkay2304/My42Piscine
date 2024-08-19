ffd55a9a4e89242bfe0706a273a4c6376e7f6609
Berkay2304 <cansever.berkay23@gmail.com>
2024-08-20 02:36:02 +0300

    son commit

--- shell_01/ex07/r_dwssap.sh
+++ shell_01/ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /etc/passwd | awk -F: 'NR % 2 == 0 {print $1}' | awk -v FT_LINE1="$FT_LINE1" -v FT_LINE2="$FT_LINE2" 'NR >= FT_LINE1 && NR <= FT_LINE2 {print $0}' | sort -r | rev | tr '\n' ', ' | tr -d '\n' | sed 's/,$/./g'
+cat /etc/passwd | awk -F: 'NR % 2 == 0 {print $1}' | rev | sort -r | awk -v FT_LINE1="$FT_LINE1" -v FT_LINE2="$FT_LINE2" 'NR >= FT_LINE1 && NR <= FT_LINE2 {print $0}' | tr '\n' ', ' | tr -d '\n' | sed 's/,$/./g'
---
+++ shell_01/ex08/add_chelou.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+FT_NBR1=5
+FT_NBR2=5
+echo "$FTNBR2"
