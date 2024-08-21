34fc8336123065295a908feb0a45b1f03fc04cb2
Berkay2304 <cansever.berkay23@gmail.com>
2024-08-22 01:26:45 +0300

    ex08 yapıldı

--- shell_01/ex08/add_chelou.sh
+++ shell_01/ex08/add_chelou.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo $FT_NBR1 = \'\?\"\\\"\'\\ + $FT_NBR2=rcrdmddd | sed "s/\'/0/g" | tr '\\\"\?\!' 1234 | tr 'mrdoc' 01234 | xargs echo 'obase=13; ibase=5;' | bc | tr 0123456789ABC 'gtaio luSnemf'
+FT_NBR1="\'?\"\\\"'\\"
+FT_NBR2=rcrdmddd
+echo $FT_NBR1 + $FT_NBR2 | sed "s/'/0/g" | tr '\\\"\?\!' 1234 | tr 'mrdoc' 01234 | xargs echo 'obase=13; ibase=5;' | bc | tr 0123456789ABC 'gtaio luSnemf'
