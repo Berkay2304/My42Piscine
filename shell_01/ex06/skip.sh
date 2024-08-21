d7727ec2b30135ef0f7e151bd8c4dae6012c5002
Berkay2304 <cansever.berkay23@gmail.com>
2024-08-21 22:42:26 +0300

    ex08 hariç yapıldı

--- shell_01/ex06/skip.sh
+++ shell_01/ex06/skip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ls -l | awk 'NR%2==1'
+ls -l | awk 'NR%2==0'
--- shell_01/ex07/r_dwssap.sh
+++ shell_01/ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /etc/passwd | awk -F: 'NR % 2 == 0 {print $1}' | rev | sort -r | awk -v FT_LINE1="$FT_LINE1" -v FT_LINE2="$FT_LINE2" 'NR >= FT_LINE1 && NR <= FT_LINE2 {print $0}' | tr '\n' ', ' | tr -d '\n' | sed 's/,$/./g'
+cat /etc/passwd | awk 'NR % 2 == 0' | cut -d':' -f1 | rev | sort -r | sed -n "${FT_LINE1},${FT_LINE2}p" | tr '\n' ' ' | sed 's/ /, /g' | sed 's/, $/./' 
