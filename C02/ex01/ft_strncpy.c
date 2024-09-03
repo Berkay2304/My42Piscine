a8512eb88b3bf24db5ec4ca69e2947bdb201bc24
Berkay Cansever <bcanseve@k1m22s04.42istanbul.com.tr>
2024-09-03 14:56:24 +0300

    C02 düzeltildi

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -6,30 +6,23 @@
 /*   By: bcanseve <bcanseve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 14:51:37 by bcanseve          #+#    #+#             */
-/*   Updated: 2024/09/02 14:53:01 by bcanseve         ###   ########.fr       */
+/*   Updated: 2024/09/03 14:54:09 by bcanseve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (*(str + i) != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
 	while (i < n)
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
