65fd5c694b862770a1fc15ae4aa9df94e1e4db98
Berkay Cansever <bcanseve@k1m20s01.42istanbul.com.tr>
2024-09-04 13:44:09 +0300

    C04 yapıldı

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcanseve <bcanseve@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 13:21:58 by bcanseve          #+#    #+#             */
+/*   Updated: 2024/09/04 13:36:36 by bcanseve         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	whitespaces(char *str, int *ptr_i)
+{
+	int	sign;
+	int	i;
+
+	i = 0;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] && (str[i] == 43 || str[i] == 45))
+	{
+		if (str[i] == 45)
+			sign *= -1;
+		i++;
+	}
+	*ptr_i = i;
+	return (sign);
+}
+
+int	ft_atoi(char *str)
+{
+	int	sign;
+	int	result;
+	int	i;
+
+	result = 0;
+	sign = whitespaces(str, &i);
+	while (str[i] && str[i] >= 48 && str[i] <= 57)
+	{
+		result *= 10;
+		result += str[i] - 48;
+		i++;
+	}
+	result *= sign;
+	return (result);
+}
